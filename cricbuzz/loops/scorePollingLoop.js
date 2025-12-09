b875f7e4e4d0ad113e6543850f2cb3e369a01817
vishalpatilblog1-ai <vishal.patil.blog1@gmail.com>
2025-12-10 03:31:42 +0530

    Update scorePollingLoop.js

--- cricbuzz/loops/scorePollingLoop.js
+++ cricbuzz/loops/scorePollingLoop.js
@@ -265,7 +265,7 @@ export async function scorePollingLoop(MATCH_ID, MATCH_NAME) {
       if (evTeamMilestone) events.push(evTeamMilestone);
       if (evBatsmanMilestone) events.push(evBatsmanMilestone);
       if (evSix) events.push(evSix);
-     // if (evFour) events.push(evFour);
+      if (evFour) events.push(evFour);
       if (evMaidenOver) events.push(evMaidenOver);
 
       // if (evDefault) events.push(evDefault);
